6e425ac2379de60b93a1778c505500bed21dcb9f
Daniel Martinez <danielmartinez@daniels-mbp.lan>
2022-04-17 18:52:57 +1000

    fourth commit

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -7,10 +7,21 @@ class ManagersController < ApplicationController
     @manager =Manager.new
   end
 
+  def create
+    manager = Manager.create manager_params
+    redirect_to manager
+  end
+
   def edit
   end
 
   def show
     @manager= Manager.find params[:id]
   end
+
+  private 
+  def manager_params 
+    params.require(:manager).permit(:name, :team, :trophies, :nationality, :image)
+  end
+
 end
