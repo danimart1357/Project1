e2d2264946f418d71f9b67f0f9451dcc7d50548a
Daniel Martinez <danielmartinez@daniels-mbp.lan>
2022-04-19 01:38:54 +1000

    log out

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,11 +6,15 @@ class SessionController < ApplicationController
   user =  User.find_by :email =>params[:email]
 
   if user.present? && user.authenticate(params[:password])
-    redirect_to teams_path
+    session[:user_id] = user.id
+    redirect_to root_path
   else
     redirect_to login_path
   end
   
   end
-
+  def destroy
+    session[:user_id]=nil
+    redirect_to login_path
+  end
 end
