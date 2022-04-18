55851d6e09f352cb0c4c8a96e3b99113d0219a94
Daniel Martinez <danielmartinez@daniels-mbp.lan>
2022-04-18 23:15:24 +1000

    users n home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
  
+  root :to => 'pages#home'
+  resources :users, :only => [:new, :create]
   resources :teams
   resources :managers
   resources :players
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_18_064849) do
+ActiveRecord::Schema.define(version: 2022_04_18_130951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,12 @@ ActiveRecord::Schema.define(version: 2022_04_18_064849) do
     t.text "country"
     t.text "image"
     t.text "dob"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
