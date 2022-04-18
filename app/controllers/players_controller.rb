6acf1ada91b00dabaf20ad7710600a52461d0d96
Daniel Martinez <danielmartinez@daniels-mbp.lan>
2022-04-18 16:41:46 +1000

    players doen

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -7,10 +7,35 @@ class PlayersController < ApplicationController
     @player = Player.new
   end
 
+  def create
+    player = Player.create player_params
+    redirect_to player
+  end
+
+
   def edit
+    @player = Player.find params[:id]
+  end
+
+  def update
+    player = Player.find params[:id]
+    player.update player_params
+    redirect_to player
   end
 
   def show
     @player = Player.find params[:id]
   end
+
+  def destroy
+    player = Player.find params[:id]
+    player.destroy
+    redirect_to players_path
+  end
+
+  private
+  def player_params
+    params.require(:player).permit(:title, :year, :manager, :team, :goals, :trophy, :image)
+  end
+
 end
